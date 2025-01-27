cf56a7c6b35160a270e17c7ab391576ffc31071a
ascendingmine <avyang@s.sfusd.edu>
2025-01-27 12:56:56 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -28,33 +28,39 @@ public class PigLatin {
 	    }
     }
 public int findFirstVowel(String sWord)
-//precondition: sWord is a valid String of length greater than 0.
-//postcondition: returns the position of the first vowel in sWord.  If there are no vowels, returns -1
+  //precondition: sWord is a valid String of length greater than 0.
+  //postcondition: returns the position of the first vowel in sWord.  If there are no vowels, returns -1
 {
-      for(int i = 0; i < sWord.length(); i++)
-       if(sWord.charAt(i) == 'a' || sWord.charAt(i) == 'e' || sWord.charAt(i) == 'i' || sWord.charAt(i) == 'o' || sWord.charAt(i) == 'u')
-      return i;
+  if (sWord.length()>0) {
+    for (int i = 0; i < sWord.length(); i++)
+      if (sWord.charAt(i) == 'a' || sWord.charAt(i) == 'e' || sWord.charAt(i) == 'i' || sWord.charAt(i) == 'o' || sWord.charAt(i) == 'u')
+        return i;
+    return -1;
+  }
   return -1;
 }
 
 public String pigLatin(String sWord)
-//precondition: sWord is a valid String of length greater than 0
-//postcondition: returns the pig latin equivalent of sWord
+  //precondition: sWord is a valid String of length greater than 0
+  //postcondition: returns the pig latin equivalent of sWord
 {
-      if(findFirstVowel(sWord) == 0)
-  {
-    return sWord + "way";
+  if (sWord.length()>0) {
+    if (findFirstVowel(sWord) == 0)
+    {
+      return sWord + "way";
+    }
+    if (findFirstVowel(sWord) == -1)
+    {
+      return sWord + "ay";
+    }
+    if (sWord.substring(0, 2).equals("qu"))
+    {
+      return sWord.substring(2, sWord.length())+sWord.substring(0, 2)+"ay";
+    }  
+    if (findFirstVowel(sWord) != -1 && sWord.substring(0, 1)!="a"||sWord.substring(0, 1)!="e"||sWord.substring(0, 1)!="i"||sWord.substring(0, 1)!="o"||sWord.substring(0, 1)!="u")    
+    {
+      return sWord.substring(1, sWord.length())+sWord.substring(0, 1)+"ay";
+    }
   }
-    if(findFirstVowel(sWord) == -1)
-  {
-    return sWord + "ay";
-  }
-  if(sWord.substring(0,2).equals("qu"))
-  {
-    return sWord.substring(2,sWord.length())+sWord.substring(0,2)+"ay";
-  }  
-    if(findFirstVowel(sWord) != -1 && sWord.substring(0,1)!="a"||sWord.substring(0,1)!="e"||sWord.substring(0,1)!="i"||sWord.substring(0,1)!="o"||sWord.substring(0,1)!="u")    
-  {
-    return sWord.substring(1,sWord.length())+sWord.substring(0,1)+"ay";
-  }  
-
+  return "ERROR!";
+}
